ca7e25727f246da6704151e6a1298c6ef92005ae
namanagr <namanagr@gmail.com>
2017-07-25 23:30:55 -0700

    latest commit from thinkpad

---
+++ src/Tree/p31_print_all_ancestors_of_node_bt.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tree;
+
+/**
+ *
+ * @author namanagr
+ */
+public class p31_print_all_ancestors_of_node_bt {
+    
+    static boolean print_ancestors(TreeNode root, TreeNode node) {
+        if (root == null)
+            return false;
+        if ((root == node) || print_ancestors(root.left, node) || print_ancestors(root.right, node)) {
+            System.out.println(root.val);
+            return true;
+        }
+        return false;
+    } 
+    
+    public static void main(String[] args){
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        //root.right.right = new TreeNode(7);
+        System.out.println(print_ancestors(root, root.right.left));
+    }
+}
